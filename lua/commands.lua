49bf3b841bf462e8f59c702c148a4723baf3c29e
Omar <omarrafat60@gmail.com>
2025-03-26 00:17:04 +0300

    Added `:Lang=json` as an aliase to set the filetype for a file manually

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,4 +1,3 @@
-
 vim.api.nvim_create_user_command("W", "w", {})
 vim.api.nvim_create_user_command("Q", "q", {})
 vim.api.nvim_create_user_command("Qa", "qa", {})
@@ -8,3 +7,11 @@ vim.api.nvim_create_user_command("V", "v", {})
 vim.api.nvim_create_user_command("Vs", "vs", {})
 vim.api.nvim_create_user_command("Reload", "e!", {})
 vim.api.nvim_create_user_command("ReloadAll", "bufdo e!", {})
+
+vim.api.nvim_create_user_command("Lang", function(opts)
+    vim.cmd("set filetype " .. opts.args)
+end, {
+    nargs = 1,
+    complete = "filetype",
+    desc = "Manually set filetype",
+})
